b3fa85afde9b81fa64f8a58d7ec9e13fc496cd43
Jo <Jokershao@JoMacBook-Pro.local>
2018-12-21 10:48:16 +0800

    数据mock

--- vue.config.js
+++ vue.config.js
@@ -22,19 +22,19 @@ module.exports = {
   },
   devServer: {
     before (app) {
-      app.get('api/seller', function (req, res) {
+      app.get('/api/seller', function (req, res) {
         res.json({
           errno: 0,
           data: seller
         })
       })
-      app.get('api/goods', function (req, res) {
+      app.get('/api/goods', function (req, res) {
         res.json({
           errno: 0,
           data: goods
         })
       })
-      app.get('api/ratings', function (req, res) {
+      app.get('/api/ratings', function (req, res) {
         res.json({
           errno: 0,
           data: ratings
